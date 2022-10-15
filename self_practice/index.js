8f31cd870173c9a887ca6c6430aa29bad4aa330d
josecamacho08 <camachojoseluis08@gmail.com>
2022-10-14 23:13:05 -0500

    hw3

---
+++ self_practice/index.js
@@ -0,0 +1,22 @@
+//This is my first javascript code
+console.log('hello world')
+
+let name = 'Jose'; //string literal
+let age ='19'; //number literal
+let isApproved = true //boolean literal true or false
+let firstName= undefined;
+let selectedColor = null;
+
+console.log(name);
+
+//rules for variables
+//cannot be reserved keyword
+//meaningful names
+//cannot start with a number
+//cannot contain space or hyphen
+//case-sensitive
+
+
+const interestRate = .03;
+interestRate = 1;
+console.log(interestRate);
